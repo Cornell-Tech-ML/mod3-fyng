d4e6984696c88590126225e37b4c5c2e2db93049
fyng <feiyangthegoat@gmail.com>
2024-11-15 21:16:08 -0500

    fix map broadcast_index extra arg

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -175,7 +175,7 @@ def tensor_map(
         i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
         # TODO: Implement for Task 3.3.
         if i < out_size:
-            to_index(i, out_shape, out_strides, out_index)
+            to_index(i, out_shape, out_index)
             broadcast_index(out_index, out_shape, in_shape, in_index)
             in_pos = index_to_position(in_index, in_strides)
             out_pos = index_to_position(out_index, out_strides)
